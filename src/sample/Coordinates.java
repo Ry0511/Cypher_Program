134af1472a119ef98827b0e81f7d036277f6ffde
Ryan Payne <ryanpayne0511@gmail.com>
2021-04-29 11:39:27 +0100

    Base program

---
+++ src/sample/Coordinates.java
@@ -0,0 +1,106 @@
+package sample;
+
+/**
+ * Co-ordinate value object which allows for the storage of abstract integers
+ * values into cartesian co-ordinate values (x, y) Note this doesn't store z
+ * axis.
+ *
+ * @author -Ry
+ * @version 1.0
+ * Copyright: N/A
+ */
+public class Coordinates<T> {
+    /**
+     * x value co-ordinates (refers to the columns)
+     */
+    private int x;
+
+    /**
+     * y value co-ordinates (refers to the rows)
+     */
+    private int y;
+
+    /**
+     * Element stored/referenced at this co-ordinate
+     */
+    private T element;
+
+    /**
+     * Construct co-ordinate values from x and y input integer values.
+     * Note x refers to columns and y refers to rows.
+     *
+     * @param x x Co-ordinate -> Column Index Value.
+     * @param y y Co-ordinate -> Row Index Value.
+     */
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+        element = null;
+    }
+
+    /**
+     * @return {@code true} if the {@link #x} and {@link #y} coordinates of
+     * this object and the provided object, match exactly (refer to the same
+     * position). Else returns {@code false}
+     */
+    public boolean equals(Coordinates<T> e) {
+        boolean isXSame = this.x == e.getX();
+        boolean isYSame = this.y == e.getY();
+        return isXSame && isYSame;
+    }
+
+    /**
+     * @return Formatted string containing the x, and y, co-ordinates.
+     */
+    public String toString() {
+        return String.format("(%s, %s)", x, y);
+    }
+
+    /**
+     * @return {@link #x} co-ordinate.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Set {@link #x} co-ordinate to the provided value.
+     *
+     * @param x The provided value to set the x co-ordinate value to.
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return {@link #y} co-ordinate.
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Set {@link #y} co-ordinate to the provided value.
+     *
+     * @param y The provided value to set the y co-ordinate value to.
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return The element stored at {@link #element} for this co-ordinate.
+     */
+    public T getElement() {
+        return this.element;
+    }
+
+    /**
+     * Sets the element at {@link #element} to the provided element.
+     *
+     * @param elem The provided element to store at these co-ordinates.
+     */
+    public void setElement(T elem) {
+        this.element = elem;
+    }
+}
---
+++ src/sample/StringIterator.java
@@ -0,0 +1,83 @@
+package sample;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator implementation for usage of traversing a String char by char
+ * with the back-end code implemented to allow ease of traversal. This allows
+ * traversal to go forwards and backwards.
+ *
+ * @author -Ry
+ * @version 1.0
+ * Copyright: N/A
+ */
+public class StringIterator implements java.util.Iterator<String> {
+    private final String text;
+    private int index;
+
+    /**
+     * Construct our String Iterator, initialising the variables for the Text
+     * and Index.
+     *
+     * @param cypher Input text to be iterated through.
+     */
+    public StringIterator(String cypher) {
+        this.text = cypher;
+        this.index = 0;
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        return index < text.length();
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    @Override
+    public String next() {
+        if (hasNext()) {
+            final String out = String.valueOf(text.charAt(index));
+            index++;
+            return out;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * Returns the previous element in the iteration. (This can be used to
+     * index backwards)
+     *
+     * @return the previous element in the iteration
+     * @throws NoSuchElementException if the iteration has no previous element
+     */
+    public String previous() {
+        if (hasPrevious()) {
+            final String out = String.valueOf(text.charAt(index - 1));
+            index--;
+            return out;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    /**
+     * Returns {@code true} if the Iterator has a previous element (index can
+     * be sent backwards) to find an element in the String. Else this will
+     * return {@code false}.
+     */
+    public boolean hasPrevious() {
+        return ((index - 1) >= 0);
+    }
+}
